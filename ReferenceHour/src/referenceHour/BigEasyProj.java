514765834439e3cc4858d79c5c6e1d0a0ecdaf6e
Kahl Mathews <kahlmathews@gmail.com>
2018-10-27 21:56:42 -0700

    Add files via upload

---
+++ ReferenceHour/src/referenceHour/BigEasyProj.java
@@ -0,0 +1,20 @@
+package referenceHour;
+
+import java.util.*;
+
+public class BigEasy {
+
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter name: ");
+		String input = sc.nextLine();
+		
+		System.out.print(input);
+		
+		
+		
+	}
+	
+}
